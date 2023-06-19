4fe325f680c97f0765d370d80834433b625a156e
marcel <marcel.hoppe96@gmx.de>
2023-06-19 16:29:23 +0200

    added settings

--- src/app/modules/settings/components/settings/settings.component.ts
+++ src/app/modules/settings/components/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActionSheetController} from "@ionic/angular";
 
 @Component({
   selector: 'app-settings',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent {
+
+  constructor(
+    private actionSheetController: ActionSheetController
+  ) {}
+
+  uploadImage() {
+    console.log('upload Image')
+  }
+
+  async openSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Profilbild verwalten',
+      buttons: [{
+        text: 'Bearbeiten',
+        handler: () => {
+          this.uploadImage();
+        }
+      }, {
+        text: 'Ansehen',
+        handler: () => {
+          console.log('Item 1');
+        }
+      },     {
+        text: 'Cancel',
+        role: 'cancel',
+        data: {
+          action: 'cancel',
+        },
+      },]
+    })
+    await actionSheet.present()
+  }
 
 }
