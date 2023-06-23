47e5773acbe42a9ff940497dce2a3c82910b6388
Boran Koeruek <borankrk@gmail.com>
2023-06-24 01:43:14 +0200

    Added firebase auth, added createNewChat & sendMessage methods, updated models

--- src/app/service/firebase.service.ts
+++ src/app/service/firebase.service.ts
@@ -1,39 +1,77 @@
 import { inject, Injectable } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
 import {
+  addDoc,
   collection,
   collectionData,
-  doc,
-  docData,
-  DocumentData,
+  DocumentReference,
   Firestore,
+  limit,
   query,
+  serverTimestamp,
   where,
 } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
+import { Chat } from '../models/chat';
 import { Message } from '../models/message';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseService {
-  public readonly firestore: Firestore = inject(Firestore);
+  public static readonly firestore: Firestore = inject(Firestore);
+  public static readonly auth: Auth = inject(Auth);
 
   constructor() {}
 
-  public getChat(chatId: string): Observable<DocumentData> {
-    return docData(doc(this.firestore, 'chats', chatId));
+  public static getMessagesFromChat(chatId: string): Observable<Message[]> {
+    return collectionData(
+      query(collection(this.firestore, 'chats', chatId, 'messages'), limit(100))
+    ) as Observable<Message[]>;
   }
 
-  public getChats(userId: string): Observable<DocumentData[]> {
-    // todo: add where clause
-    return collectionData(collection(this.firestore, 'chats')) as Observable<
-      Message[]
-    >;
+  public static sendMessage(
+    text: string,
+    chatId: string
+  ): Observable<DocumentReference> {
+    const message = new Message();
+    message.senderUid = this.auth.currentUser?.uid as string;
+    message.text = text;
+    message.timestamp = serverTimestamp();
 
-    query(
-      collection(this.firestore, 'chats'),
-      where('users', 'array-contains', userId)
+    // todo: check if this delete is really needed
+    delete message.id;
+
+    return from(
+      addDoc(
+        collection(this.firestore, 'chats', chatId, 'messages'),
+        <Message>message
+      )
     );
+  }
+
+  public static getChats(userId: string): Observable<Chat[]> {
+    return collectionData(
+      query(
+        collection(this.firestore, 'chats'),
+        where('participants', 'array-contains', userId)
+      )
+    ) as Observable<Chat[]>;
+  }
+
+  public static createNewChat(
+    participantUid: string
+  ): Observable<DocumentReference> {
+    const chat = new Chat();
+    chat.participants.push(
+      this.auth.currentUser?.uid as string,
+      participantUid
+    );
+
+    // todo: check if this delete is really needed
+    delete chat.id;
+
+    return from(addDoc(collection(this.firestore, 'chats'), <Chat>chat));
   }
 
   // todo: add username search
