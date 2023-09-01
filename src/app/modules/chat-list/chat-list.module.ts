720e523564cae816332607b41d504c8d0a299515
Boran Koeruek <borankrk@gmail.com>
2023-09-01 13:07:38 +0200

    Added i18n translation

--- src/app/modules/chat-list/chat-list.module.ts
+++ src/app/modules/chat-list/chat-list.module.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
 import { ChatListRoutingModule } from './chat-list.routing.module';
+import { AddUserDialogComponent } from './components/add-user-dialog/add-user-dialog.component';
 import { ChatListComponent } from './components/chat-list/chat-list.component';
 import { ChatComponent } from './components/chat/chat.component';
-import { AddUserDialogComponent } from './components/add-user-dialog/add-user-dialog.component';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, ChatListRoutingModule],
+  imports: [CommonModule, IonicModule, ChatListRoutingModule, TranslateModule],
   declarations: [ChatComponent, ChatListComponent, AddUserDialogComponent],
-
 })
 export class ChatListModule {}
